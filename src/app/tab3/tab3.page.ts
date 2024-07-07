a728b27bb3df3985ad92cc5f5c28c79c76894d30
di-saldana <dsl42@alu.ua.es>
2024-07-07 15:42:57 +0200

    Added: Profile Card

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
+  userName: string = 'Dianelys Saldaña';
+  userAge: number = 25;
+  userDistance: number = 0; 
+  favoriteArtists: string[] = ['Lorde', 'Declan McKenna', 'Hozier'];
+  favoriteSongs: string[] = ['Unknown / Nth', 'The Key to Life on Earth', 'Team'];
+  favoriteGenres: string[] = ['Indie pop', 'Alternative', 'Latin rock'];
 
   constructor() {}
+
+  ngOnInit() {
+    // Initialize or fetch user data here
+  }
 
 }
