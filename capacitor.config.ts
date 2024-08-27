4d645b604d4b9eebccfccb62994f775447494797
di-saldana <dsl42@alu.ua.es>
2024-08-27 23:17:26 +0200

    Updated: Capacitor ios files

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'es.ua.mastermoviles.dsl.tfm',
-  appName: 'TFM App',
+  appName: 'ConcertPal',
   webDir: 'www'
 };
 
